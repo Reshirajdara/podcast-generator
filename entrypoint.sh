c6d79e00c3f662fbb307de93aaa99f800b59623f
Reshi <rajreshi96@gmail.com>
2024-05-21 16:35:49 -0500

    added action.yml

---
+++ entrypoint.sh
@@ -0,0 +1,14 @@
+echo "==================================="
+
+git config --global user.name "${GITHUB_ACTOR}"
+git config --global user.email "${INPUT_EMAIL}"
+git config --global --add safe.directory /github/workspace
+
+
+python3 /urs/bin/feed.py
+
+git ass -A && git commit -m "Update Feed"
+
+git push --set-upstream origin main
+
+echo "==================================="
